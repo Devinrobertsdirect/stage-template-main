790eac02619a9ff6dc255783d9962cc897f3f9b0
Devin_Roberts <DCRoberts1@student.fullsail.edu>
2024-11-11 10:14:09 -0500

    test2

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // You can uncomment these if you want to follow the
+  // You can uncomment these if you wants to follow the
   //    'always render twice in dev mode' convention,
   //     which is common enough to be worth leaving in
   //     commented-out code, but because of the nature
